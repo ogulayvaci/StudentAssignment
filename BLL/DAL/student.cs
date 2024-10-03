02658c6328ab4dad99a30a0aa560cdbefeba00a0
Ogul Ayvaci <ogul.ayvaci@ug.bilkent.edu.tr>
2024-10-03 16:56:39 +0300

    .Net Assignment #1

---
+++ BLL/DAL/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BLL.DAL;
+
+public partial class student
+{
+    public int id { get; set; }
+
+    public string name { get; set; } = null!;
+
+    public string surname { get; set; } = null!;
+
+    public DateTime? birthdate { get; set; }
+
+    public decimal? gpa { get; set; }
+}
